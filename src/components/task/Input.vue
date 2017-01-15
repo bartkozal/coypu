14bf2b65c9d597d29b9782a1aba4e441e99fc8ff
Bartlomiej Kozal <bkzl@me.com>
2017-01-15 13:58:00 +0100

    Debounce save on blur

--- src/components/task/Input.vue
+++ src/components/task/Input.vue
@@ -52,9 +52,9 @@ export default {
     }
   },
   methods: {
-    blur: function () {
+    blur: debounce(function () {
       this.$store.dispatch('saveActiveList')
-    },
+    }, 200),
     deselectTask () {
       this.$refs.textarea.blur()
       this.$store.commit('selectTask', null)
