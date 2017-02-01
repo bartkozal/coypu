16aa3471085cd542ab7c15633a3793e43d4795b7
Bartlomiej Kozal <bkzl@me.com>
2017-02-01 19:56:45 +0100

    Disable undo and redo

--- app/menu.js
+++ app/menu.js
@@ -5,15 +5,6 @@ const template = [
     label: 'Edit',
     submenu: [
       {
-        role: 'undo'
-      },
-      {
-        role: 'redo'
-      },
-      {
-        type: 'separator'
-      },
-      {
         role: 'cut'
       },
       {
